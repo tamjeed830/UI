aae45368a695badb5eb17ac4b251424fc08aae1c
Mohammed Tamjeed <Mohammed Tamjeed@LAPTOP-EP89R35M>
2024-05-23 17:51:21 -0500

    Initialize project using Create React App

---
+++ src/bloglist.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5258/blogs')
+      .then(response => setBlogs(response.data))
+      .catch(error => console.error('Error fetching data: ', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Blogs</h1>
+      {blogs.map(blog => (
+        <div key={blog.blogId}>
+          <h2>{blog.title}</h2>
+          <p>{blog.content}</p>
+          <small>By {blog.author} on {new Date(blog.publishedDate).toLocaleDateString()}</small>
+        </div>
+      ))}
+    </div>
+  );
